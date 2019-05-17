2b81d78ffb15b53a66cabf0b3571cd7008bdb184
Mathew Henshall <mat.henshall@rumbleship.com>
2019-05-17 15:14:49 -0700

    build lib

--- lib/db/transformers.d.ts
+++ lib/db/transformers.d.ts
@@ -5,6 +5,7 @@ import { ClassType } from '../helpers/classtype';
 export declare const modelKey: unique symbol;
 export declare const apiKey: unique symbol;
 export declare function modelToClass<T extends Node<T>, V extends Model<V>>(nodeService: NodeService<T>, to: ClassType<T>, from: V): T;
+export declare function reloadNodeFromModel<T extends Node<T>>(node: T, fromDb?: boolean): Promise<T>;
 export declare function convertToSequelizeEnum<TEnum>(toConvert: TEnum, options?: {
     exclude: string[];
 }): import("sequelize/types").EnumDataType<string>;
