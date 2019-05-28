1c5621182b2a3d2994a8c9390f246fa4101d5443
Mathew Henshall <mat.henshall@rumbleship.com>
2019-05-28 14:04:30 -0700

    ...and the build

--- lib/db/index.d.ts
+++ lib/db/index.d.ts
@@ -4,3 +4,4 @@ export * from './sequelize-base.service';
 export * from './transformers';
 export * from './validate-from-exemplar';
 export * from './date-range-filter';
+export * from './add_node_services_to_container';
