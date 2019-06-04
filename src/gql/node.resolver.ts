a84be7a8c754919e5c1226e29d1cba0be4f44876
Mathew Henshall <mat.henshall@rumbleship.com>
2019-06-04 11:11:16 -0700

    Added subscriptions to the generic 'node'

--- src/gql/node.resolver.ts
+++ src/gql/node.resolver.ts
@@ -1,7 +1,39 @@
 import { Service, Inject } from 'typedi';
-import { Resolver, Query, ID, Arg, Ctx } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  ID,
+  Arg,
+  Ctx,
+  Subscription,
+  Field,
+  Int,
+  ObjectType,
+  Root
+} from 'type-graphql';
 import { Node, RelayResolver, Oid } from './index';
 import { NodeService } from './relay.service';
+import {
+  GqlNodeNotification,
+  NodeNotification,
+  NotificationOf,
+  NODE_CHANGE_NOTIFICATION,
+  DbModelChangeNotification
+} from './node-notification';
+
+// we make a specific concreate type here for the concrete general Node notification
+@ObjectType()
+class ClassGqlNodeNotification extends NodeNotification<any> {
+  @Field(type => Int)
+  sequence!: number;
+  @Field(type => NotificationOf)
+  notificationOf!: NotificationOf;
+  @Field(type => Node, { nullable: true })
+  node!: Node<any>;
+  constructor(notificationOf: NotificationOf, node: Node<any>) {
+    super(notificationOf, node);
+  }
+}
 
 @Service()
 @Resolver()
@@ -21,7 +53,26 @@ export class NodeResolver implements RelayResolver {
     }
     throw Error('Invalid OID. Scope:' + scope);
   }
-
+  @Subscription(type => ClassGqlNodeNotification, {
+    name: `onNodeChange`,
+    topics: `${NODE_CHANGE_NOTIFICATION}`,
+    nullable: true
+  })
+  async onChange(@Root() payload: DbModelChangeNotification): Promise<ClassGqlNodeNotification> {
+    // convert to GQL Model
+    const modelId: string = payload.model.get('id') as string;
+    // ASSUME that the db model is suffixed with Model
+    const gqlModelName = payload.model.constructor.name.slice(0, 5);
+    const oid = Oid.create(gqlModelName, modelId);
+    const { scope } = oid.unwrap();
+    if (scope in this.nodeServices) {
+      const node = Reflect.get(this.nodeServices, scope).getOne(oid);
+      const gqlNodeNotification = new ClassGqlNodeNotification(payload.notificationOf, node);
+      return gqlNodeNotification;
+    } else {
+      throw Error('Invalid OID. Scope:' + scope);
+    }
+  }
   // for developers and system support,
   @Query(returns => String)
   async unWrapOid(@Arg('id', type => ID) oidString: string, @Ctx() ctx: any): Promise<string> {
