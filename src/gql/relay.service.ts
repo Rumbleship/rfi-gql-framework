8107d910c533688c231f91d19f48c1752c316d3d
Mathew Henshall <mat.henshall@rumbleship.com>
2019-06-06 10:11:49 -0700

    removed experimental pub-sub injection on create/ update (final implementation does it using hooks)

--- src/gql/relay.service.ts
+++ src/gql/relay.service.ts
@@ -20,8 +20,8 @@ export interface RelayService<
   count(filterBy: TFilter): Promise<number>;
   findOne(filterBy: TFilter, paranoid?: boolean): Promise<TApi | null>;
   getOne(oid: Oid): Promise<TApi>;
-  create(data: TInput, pubSub?: PubSubEngine): Promise<TApi>;
-  update(data: TUpdate, pubSub?: PubSubEngine): Promise<TApi>;
+  create(data: TInput): Promise<TApi>;
+  update(data: TUpdate): Promise<TApi>;
   getAssociatedMany<
     TAssocApi extends Node<TAssocApi>,
     TAssocConnection extends Connection<TAssocApi>,
