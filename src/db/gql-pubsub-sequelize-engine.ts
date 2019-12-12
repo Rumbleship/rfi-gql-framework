c356aee0cb6c50e3389b01537a28911087aaf4fa
Samson Yeung <samson@stripe.com>
2019-12-12 13:40:52 -0800

    publishging; need to auto-create topic

--- src/db/gql-pubsub-sequelize-engine.ts
+++ src/db/gql-pubsub-sequelize-engine.ts
@@ -8,6 +8,72 @@ import {
   ModelDelta
 } from '../gql/node-notification';
 
+
+// For creating topics which don't exist
+//import { GooglePubSub as PubSub } from '@axelspringer/graphql-google-pubsub';
+import { PubSub as GooglePubSub } from '@google-cloud/pubsub';
+
+//const {PubSub} = require('@google-cloud/pubsub');
+
+//FIXME - do better than 'any'
+// the issue is 'publish' 404's on the pubSubEngine obj
+const googlePubSub: any = new GooglePubSub();
+
+//async googlePubSub.createTopic(NODE_CHANGE_NOTIFICATION);
+// FIXME - this is racey
+
+
+var topicList: string[] = [];
+
+
+async function listAllTopics() {
+  // Lists all topics in the current project
+  const [topics] = await googlePubSub.getTopics();
+  
+  topics.forEach((topic: any) => {
+    const i = topic.name.lastIndexOf("/")+1;
+    const topicShortName = topic.name.slice(i);
+    topicList.push(topicShortName);
+  });
+  
+  
+  //topicList.push(...topics);
+
+  //console.log('Topics:');
+  // FIXME - do better than 'any'
+  //topics.forEach((topic: any) => console.log(topic.name));
+
+  console.log('topicList has', topicList);
+}
+
+function setupGooglePubSub() {
+  listAllTopics();
+}
+
+setupGooglePubSub();
+
+// FIXME - this may be racy
+
+// need to do this using promise/await/async/whatever
+//googlePubSub.createTopic(NODE_CHANGE_NOTIFICATION);
+
+
+
+/*
+function getTopic(cb) {
+  pubsub.createTopic(topicName, (err, topic) => {
+    // topic already exists.
+    if (err && err.code === 6) {
+      cb(null, pubsub.topic(topicName));
+      return;
+    }
+    cb(err, topic);
+  });
+}
+*/
+
+
+
 /**
  *
  */
@@ -90,10 +156,13 @@ function gqlCreateHook(
   deltas: ModelDelta[],
   options: CreateOptions
 ) {
-  const payload = new DbModelChangeNotification(NotificationOf.CREATED, instance, deltas);
-  pubSub.publish(NODE_CHANGE_NOTIFICATION, payload);
-  // Also publish the specific Model
-  pubSub.publish(`${NODE_CHANGE_NOTIFICATION}_${instance.constructor.name}`, payload);
+
+  publishPayload(instance);
+
+  // const payload = new DbModelChangeNotification(NotificationOf.CREATED, instance, deltas);
+  // pubSub.publish(NODE_CHANGE_NOTIFICATION, payload);
+  // // Also publish the specific Model
+  // pubSub.publish(`${NODE_CHANGE_NOTIFICATION}_${instance.constructor.name}`, payload);
 }
 function gqlUpdateHook(
   pubSub: PubSubEngine,
@@ -101,8 +170,76 @@ function gqlUpdateHook(
   deltas: ModelDelta[],
   options: UpdateOptions
 ) {
-  const payload = new DbModelChangeNotification(NotificationOf.UPDATED, instance, deltas);
-  pubSub.publish(NODE_CHANGE_NOTIFICATION, payload);
-  // Also publish the specific Model
-  pubSub.publish(`${NODE_CHANGE_NOTIFICATION}_${instance.constructor.name}`, payload);
+  publishPayload(instance);
+  //const payload = new DbModelChangeNotification(NotificationOf.UPDATED, instance, deltas);
+  //pubSub.publish(NODE_CHANGE_NOTIFICATION, payload);
+  //// Also publish the specific Model
+  //pubSub.publish(`${NODE_CHANGE_NOTIFICATION}_${instance.constructor.name}`, payload);
 }
+
+// go into
+// https://github.com/googleapis/nodejs-pubsub/blob/master/samples/topics.js
+// and add retry logic to the below
+
+function getPayload(obj: Model): string {
+  console.log('obj')
+  console.log(obj)
+
+  var className = 'unknown'
+  var objectId = 'no id'
+  if (typeof obj !== 'undefined') {
+    className = obj.constructor.name
+    objectId = obj.get('id') as string
+  }
+  return JSON.stringify({'class': className, 'id': objectId});
+}
+
+function publishPayload(rawPayload: Model): void {
+
+  const payload: string = getPayload(rawPayload)
+
+  const topicName: string = 
+        `${NODE_CHANGE_NOTIFICATION}_${rawPayload.constructor.name}`
+  console.log(topicName, 'in pub payload');
+
+  function publish(): void {
+    googlePubSub.publish(NODE_CHANGE_NOTIFICATION, payload);
+    // Also publish the specific Model
+    googlePubSub.publish(topicName, payload);
+  }
+
+  if (!topicList.includes(topicName)) {
+    console.log(topicName, 'Is miSSING!\nCreating...')
+    googlePubSub.createTopic(topicName, (err: any, topic: string) => {
+      console.log("createTopic callbedback");
+      topicList.push(topicName);
+      if (err && err.code != 6) {
+        // FIXME - log to platform logging system
+        console.log("could not create topic");
+        publish();
+      }
+    });
+    return;
+  }
+  publish()
+
+/*
+
+  // FIXME - can we do better for 'err' than 'any'? 
+  // has 'code' property that is unknown on 'object'
+  googlePubSub.createTopic(topicName, (err: any, topic: string) => {
+    console.log('creating topic, got', err.code, 'for', topic);
+    // topic already exists.
+    if (err && err.code === 6) {
+      publish();
+      return;
+    }
+    publish();
+  })
+  */
+
+  //pubSub.publish(NODE_CHANGE_NOTIFICATION, payload);
+  //// Also publish the specific Model
+  //pubSub.publish(`${NODE_CHANGE_NOTIFICATION}_${instance.constructor.name}`, payload);
+}
+
