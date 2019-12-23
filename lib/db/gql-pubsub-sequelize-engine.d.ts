f271e2105903dd97109d1c493acd9fa62199d491
Samson Yeung <samson@stripe.com>
2019-12-23 14:03:11 -0800

    update built artifacts

--- lib/db/gql-pubsub-sequelize-engine.d.ts
+++ lib/db/gql-pubsub-sequelize-engine.d.ts
@@ -1,15 +1,9 @@
 import { RfiPubSubEngine } from '../pubsub';
 import { Sequelize } from 'sequelize-typescript';
 import { Model } from 'sequelize';
-export declare function pubSubFrom(sequelize: Sequelize): RfiPubSubEngine | null;
-/**
-import {
-  attachPubSubEngineToSequelize,
-//  pub
-} from '../src/pubsub/helper';
-**/
 /**
  *
  */
 export declare function linkSequelizeToPubSubEngine(pubSub: RfiPubSubEngine, sequelize: Sequelize): void;
 export declare function publishCurrentState(instance: Model<any, any>): void;
+export declare function pubSubFrom(sequelize: Sequelize): RfiPubSubEngine | null;
--- lib/gql/node.resolver.d.ts
+++ lib/gql/node.resolver.d.ts
@@ -1,11 +1,20 @@
 import { PubSubEngine } from 'type-graphql';
 import { Node, RelayResolver } from './index';
 import { NodeService } from './relay.service';
+import { NodeNotification, NotificationOf, DbModelChangeNotification } from './node-notification';
+declare class ClassGqlNodeNotification extends NodeNotification<any> {
+    sequence: number;
+    notificationOf: NotificationOf;
+    node: Node<any>;
+    constructor(notificationOf: NotificationOf, node: Node<any>);
+}
 export declare class NodeResolver implements RelayResolver {
     private readonly nodeServices;
     constructor(nodeServices: Array<NodeService<any>>);
     node(oidString: string, ctx: any): Promise<Node<any> | null>;
     publishLastKnownState(oidString: string, pubSub: PubSubEngine, ctx: any): boolean;
+    onChange(payload: DbModelChangeNotification): Promise<ClassGqlNodeNotification>;
     unWrapOid(oidString: string, ctx: any): Promise<string>;
     makeOid(scope: string, id: string, ctx: any): Promise<string>;
 }
+export {};
