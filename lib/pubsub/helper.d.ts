bcf6a7855469ab768585a12bfc71756767ec4f0b
Samson Yeung <samson@stripe.com>
2019-12-24 00:28:46 -0800

    update build artifacts

--- lib/pubsub/helper.d.ts
+++ lib/pubsub/helper.d.ts
@@ -1,7 +1,11 @@
+import { Oid } from '@rumbleship/oid';
 import { NodeNotification } from '../gql/node-notification';
 import { ClassType } from './../helpers/classtype';
-export declare function uniqueSubscriptionNamePart(): string;
-interface GetModelFromStrOid {
+export declare function uniqueSubscriptionNamePart(topicName: string): string;
+interface OIDPayloadCreator {
+    getOne(id: Oid): Promise<any>;
+}
+interface StrPayloadCreator {
     getOne(id: string): Promise<any>;
 }
 export interface RawPayload {
@@ -9,5 +13,6 @@ export interface RawPayload {
         toString(): string;
     };
 }
-export declare function nodeNotficationFromPayload(rawPayload: any, resolver: GetModelFromStrOid, notificationClsType: ClassType<any>): Promise<NodeNotification<any>>;
+export declare function createPayloadUsingStr(rawPayload: RawPayload, resolver: StrPayloadCreator, notificationClsType: ClassType<any>): Promise<NodeNotification<any>>;
+export declare function createPayloadUsingOid(rawPayload: RawPayload, resolver: OIDPayloadCreator, notificationClsType: ClassType<any>): Promise<NodeNotification<any>>;
 export {};
