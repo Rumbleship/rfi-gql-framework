3b1cd9b154f4342deca887300b35bd9e77e1afab
Isaac Sparling <223606+isparling@users.noreply.github.com>
2020-06-25 16:29:47 -0700

    :hear_no_evil: missed the export

--- src/app/server/index.ts
+++ src/app/server/index.ts
@@ -3,3 +3,4 @@ export * from './init-sequelize';
 export * from './rfi-pub-sub-engine';
 export * from './rfi-pub-sub-engine.interface';
 export * from './middleware';
+export * from './init-server';
