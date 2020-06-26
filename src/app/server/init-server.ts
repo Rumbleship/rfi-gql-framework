c96fa15c327c50ec397564816a25a21458e882d6
Isaac Sparling <223606+isparling@users.noreply.github.com>
2020-06-26 13:37:02 -0700

    Revert ":gift: Experiment without passing singleton FactoryMap"
    This reverts commit c481c97a3f035fc6738bc5d8d87f0701e94cdb93.

--- src/app/server/init-server.ts
+++ src/app/server/init-server.ts
@@ -15,6 +15,7 @@ import {
 } from '@rumbleship/apollo-server-hapi';
 import { RfiPubSubConfig, RumbleshipDatabaseOptions } from '@rumbleship/config';
 import { RumbleshipContextControl, getRumbleshipContextFrom } from '@rumbleship/context-control';
+import { ServiceFactories, ServiceFactoryMap } from '@rumbleship/service-factory-map';
 import { spyglassHapiPlugin, logging } from '@rumbleship/spyglass';
 import { RumbleshipBeeline, HoneycombMiddleware, addGaeVersionDataToTrace } from '@rumbleship/o11y';
 import { RumbleshipContext } from './../rumbleship-context';
@@ -54,7 +55,7 @@ export async function initServer(
   injected_models: DbModelAndOidScope[],
   injected_schema_options: Omit<BuildSchemaOptions, 'authChecker' | 'pubSub' | 'container'>,
   injected_routes: Hapi.ServerRoute[] = [],
-  onContainer: (context: RumbleshipContext) => void,
+  onContainer: (context: RumbleshipContext, ServiceFactories: ServiceFactoryMap) => void,
   onInitialized: (server: Hapi.Server) => Promise<void> = (_server: Hapi.Server) =>
     Promise.resolve(),
   dbOptions?: {
@@ -206,7 +207,7 @@ export async function initServer(
       if (container && onContainer) {
         // Hook to to allow Banking to continue inject/hook up its old services when the context is built.
         // Probably shouldnt use this for anything else.
-        onContainer(rumbleship_context);
+        onContainer(rumbleship_context, ServiceFactories);
       }
       return rumbleship_context;
     },
