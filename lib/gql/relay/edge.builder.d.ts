b7206fc7c0f053cfb399893b787041de1677a123
Mathew Henshall <mat.henshall@rumbleship.com>
2020-06-26 11:34:36 -0700

    ### Added   * buildEdgeClass() and buildConnectionClass():       creates base classes for graphql Relay Connection and Edges. Supports using graphql Interface class for the creation of the schema, and using the concrete class for the typescript implementation ### Deprecated   * GQLEdge and GQLConnection in favour of buildEdgeClass() and buildConnectionClass()

--- lib/gql/relay/edge.builder.d.ts
+++ lib/gql/relay/edge.builder.d.ts
@@ -1,4 +1,9 @@
 import { ClassType } from '../../helpers';
 import { AttribType } from './attrib.enum';
 import { Node, Edge } from './relay.interface';
+/**
+ * @deprecated in favour of @see buildEdgeClass
+ * @param TEdge
+ * @param attribType
+ */
 export declare function GQLEdge<T extends Node<T>>(TEdge: ClassType<T>, attribType?: AttribType): ClassType<Edge<T>>;
