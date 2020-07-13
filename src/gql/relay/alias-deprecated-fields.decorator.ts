c1239fd0d346cab0bea2b3c601f5ce674fd49758
Isaac Sparling <223606+isparling@users.noreply.github.com>
2020-07-13 12:11:31 -0700

    :new_moon: Need to set default on retrieval

--- src/gql/relay/alias-deprecated-fields.decorator.ts
+++ src/gql/relay/alias-deprecated-fields.decorator.ts
@@ -9,18 +9,21 @@ export function AliasDeprecatedField(
   field_options: FieldOptions = {}
 ): PropertyDecorator {
   return (obj: object, deprecated_field_prop_name: string | symbol) => {
-    const map = Reflect.getMetadata(AliasDeprecatedFieldMap, obj) ?? new Map<string, string>();
+    const map: Map<string | symbol, string> =
+      Reflect.getMetadata(AliasDeprecatedFieldMap, obj) ?? new Map<string | symbol, string>();
     map.set(deprecated_field_prop_name, new_prop_name);
     if (!field_options.deprecationReason) {
       field_options.deprecationReason = `Deprecated in favor of \`${new_prop_name}\``;
     }
+    Reflect.defineMetadata(AliasDeprecatedFieldMap, map, obj);
     return Field(field_options)(obj, deprecated_field_prop_name);
   };
 }
 
 export function TransposeDeprecatedFields(): ParameterDecorator {
   return (obj: object, property_name: string | symbol, index: number) => {
-    const map = Reflect.getMetadata(AliasDeprecatedFieldMap, obj);
+    const map: Map<string | symbol, string> =
+      Reflect.getMetadata(AliasDeprecatedFieldMap, obj) ?? new Map<string, string>();
     for (const [deprecated_field_prop_name, new_prop_name] of map.entries()) {
       const deprecated_field_val = Reflect.get(obj, deprecated_field_prop_name);
       Reflect.set(obj, new_prop_name, deprecated_field_val);
