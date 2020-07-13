2dc412f7a1639fef45b66ee78f973b96dfc625cb
Isaac Sparling <223606+isparling@users.noreply.github.com>
2020-07-13 12:53:49 -0700

    :mask: Alias decorator that is applied to _new_ properties

--- lib/gql/relay/alias-deprecated-fields.decorator.d.ts
+++ lib/gql/relay/alias-deprecated-fields.decorator.d.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
 import { FieldOptions } from 'type-graphql';
 export declare function AliasDeprecatedField(new_prop_name: string, field_options?: FieldOptions): PropertyDecorator;
+export declare function AliasFromDeprecatedField(deprecated_prop_name: symbol | string, field_options: FieldOptions): PropertyDecorator;
 export declare function TransposeDeprecatedFields(): ParameterDecorator;
--- src/gql/relay/alias-deprecated-fields.decorator.ts
+++ src/gql/relay/alias-deprecated-fields.decorator.ts
@@ -16,7 +16,50 @@ export function AliasDeprecatedField(
       field_options.deprecationReason = `Deprecated in favor of \`${new_prop_name}\``;
     }
     Reflect.defineMetadata(AliasDeprecatedFieldMap, map, obj);
-    return Field(field_options)(obj, deprecated_field_prop_name);
+    // Object.defineProperty(obj, new_prop_name, { get: val => Reflect.get() });
+    Field(field_options)(obj, deprecated_field_prop_name);
+  };
+}
+
+export function AliasFromDeprecatedField(
+  deprecated_prop_name: symbol | string,
+  field_options: FieldOptions
+): PropertyDecorator {
+  return (obj: object, new_prop_name: symbol | string) => {
+    // Copy values from old+new values to internal, "private" properties.
+    Object.defineProperty(obj, `__${String(new_prop_name)}`, Reflect.get(obj, new_prop_name));
+    Object.defineProperty(
+      obj,
+      `__${String(deprecated_prop_name)}`,
+      Reflect.get(obj, deprecated_prop_name)
+    );
+
+    // Replace new prop with getter that reads from private new-prop, else private old-prop
+    // Setter sets private new-prop
+    Object.defineProperty(obj, new_prop_name, {
+      get() {
+        const val_from_deprecated_prop = Reflect.get(obj, `__${String(deprecated_prop_name)}`);
+        const new_value = Reflect.get(obj, `__${String(new_prop_name)}`);
+        return new_value ?? val_from_deprecated_prop;
+      },
+      set(val) {
+        Reflect.set(obj, `__${String(new_prop_name)}`, val);
+      }
+    });
+    Object.defineProperty(obj, deprecated_prop_name, {
+      get() {
+        return Reflect.get(obj, `__${String(deprecated_prop_name)}`);
+      },
+      set(val) {
+        // If new prop isn't in use, when setting deprecated prop, set new prop as well
+        if (!Reflect.get(obj, `__${String(new_prop_name)}`)) {
+          Reflect.set(obj, `__${String(new_prop_name)}`, val);
+        }
+        Reflect.set(obj, `__${String(deprecated_prop_name)}`, val);
+      }
+    });
+
+    return Field(field_options)(obj, new_prop_name);
   };
 }
 
@@ -30,3 +73,4 @@ export function TransposeDeprecatedFields(): ParameterDecorator {
     }
   };
 }
+
