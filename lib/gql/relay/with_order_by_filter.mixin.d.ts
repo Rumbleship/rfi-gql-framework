5f320f074e43380901602d48aa00a64977529640
Rumbleship Operations Machine User <37818125+rumbleship-ops@users.noreply.github.com>
2020-07-17 20:42:15 +0000

    Release version 10.1.5 [skip ci]

--- lib/gql/relay/with_order_by_filter.mixin.d.ts
+++
@@ -1,8 +0,0 @@
-import { RelayOrderBy } from '../scalars/relay-order-by.scalar';
-import { ClassType } from '../../helpers';
-export declare function withOrderByFilter<TFilterBase extends ClassType<any>>(Base: TFilterBase): {
-    new (...args: any[]): {
-        [x: string]: any;
-        order_by?: RelayOrderBy<any> | undefined;
-    };
-} & TFilterBase;
