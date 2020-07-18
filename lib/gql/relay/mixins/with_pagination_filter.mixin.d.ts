338a75506b5430d0df13c00ecd2a74a2949fa093
Rumbleship Operations Machine User <37818125+rumbleship-ops@users.noreply.github.com>
2020-07-18 00:43:31 +0000

    Release version 10.1.6 [skip ci]

--- lib/gql/relay/mixins/with_pagination_filter.mixin.d.ts
+++ lib/gql/relay/mixins/with_pagination_filter.mixin.d.ts
@@ -1,7 +1,6 @@
 import { ClassType } from '../../../helpers/classtype';
-export declare function withPaginationFilter<TFilterBase extends ClassType<any>>(Base: TFilterBase): {
+export declare function withPaginationFilter<TFilterBase extends ClassType<object>>(Base: TFilterBase): {
     new (...args: any[]): {
-        [x: string]: any;
         first?: number | undefined;
         after?: string | undefined;
         last?: number | undefined;
