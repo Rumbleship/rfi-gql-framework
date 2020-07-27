6f36aaa12846dae1545c0a6c82f50b1b38c77240
Mathew Henshall <mat.henshall@rumbleship.com>
2020-07-27 16:44:49 -0700

    throw error if a change_uuid is not set

--- src/gql/relay/node-notification.ts
+++ src/gql/relay/node-notification.ts
@@ -12,6 +12,9 @@ export abstract class NodeNotification<T extends Node<T>> {
   notificationOf: NotificationOf;
   node: T;
   constructor(notificationOf: NotificationOf, change_uuid: string, node: T) {
+    if (!change_uuid) {
+      throw Error(`Must have a change_uuid set on change of ${node.constructor.name}`);
+    }
     this.notificationOf = notificationOf;
     this.node = node;
     this.change_uuid = change_uuid;
