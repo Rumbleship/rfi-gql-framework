29385a2fe35e7e7af2212a2dcac88fbc758f7171
Isaac Sparling <223606+isparling@users.noreply.github.com>
2020-08-08 16:45:59 -0700

    :ice_cream: Refactor to attach metadata to class, not property

--- src/gql/relay/filterable-id.decorator.ts
+++ src/gql/relay/filterable-id.decorator.ts
@@ -1,12 +1,15 @@
 export const FilterableIdSymbol = Symbol('FilterableIdSymbol');
 export function FilterableId(options: object = {}): PropertyDecorator {
-  return (target_class: object, property_name: string | symbol) => {
-    Reflect.defineMetadata(FilterableIdSymbol, true, target_class, property_name);
+  return (target_class, property_name) => {
+    const list: Array<string | symbol> =
+      Reflect.getMetadata(FilterableIdSymbol, target_class) ?? [];
+    list.push(property_name);
+    Reflect.defineMetadata(FilterableIdSymbol, list, target_class);
   };
 }
 export function isFilterableId(filter: any, property_name: string | symbol): boolean {
-  const filterable = Reflect.getMetadata(FilterableIdSymbol, filter, property_name);
-  if (filterable) {
+  const list = Reflect.getMetadata(FilterableIdSymbol, filter) ?? [];
+  if (list.includes(property_name)) {
     return true;
   }
   return false;
