b924767b0321798902f716a26e2df973798ec09f
Isaac Sparling <223606+isparling@users.noreply.github.com>
2020-09-21 11:29:49 -0700

    :vertical_traffic_light: use new pubsub-invoker account

--- src/queued-subscription-server/helpers/gcp_helpers.ts
+++ src/queued-subscription-server/helpers/gcp_helpers.ts
@@ -23,7 +23,7 @@ export async function gcpCreatePushSubscription(
   topic: Topic,
   subscription_name: string,
   subscription_url: string,
-  service_account_email = 'rfi-development@appspot.gserviceaccount.com'
+  service_account_email = 'pubsub-invoker@rfi-development.iam.gserviceaccount.com'
 ): Promise<Subscription> {
   const options = {
     pushConfig: {
